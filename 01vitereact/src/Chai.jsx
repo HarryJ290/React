226255075363fbb00948b3c799817c9a6705d4b7
harryJ290 <harshjadhav0120@gmail.com>
2025-07-21 21:42:14 +0530

    Learning react with vite part 1

---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,12 @@
+function Chai(){
+    return (
+       <>
+        <h1>Hello from vite chai</h1>
+       </> 
+       
+       
+    );
+}
+export default Chai;
+
+//eka time la ek element return karu shakto.mhnun <>   </> kara mg tyat kahihi taka kitihi taka.
